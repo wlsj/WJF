1edf82af6784a2718f4f25fced3ca6ea172b4bd4
Administrator <wlsgithub@126.com>
2017-02-05 14:48:53 +0800

    杨帆提交的

--- app/src/main/java/wx/eigle/yf/com/mafengwo_app/Entity/xx.java
+++ app/src/main/java/wx/eigle/yf/com/mafengwo_app/Entity/xx.java
@@ -1,5 +1,7 @@
 package wx.eigle.yf.com.mafengwo_app.Entity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +9,9 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class xx extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
