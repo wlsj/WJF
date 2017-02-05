5c70cb1cd3abbb2b4e99e4d42a8720de1b6e7ef3
Administrator <wlsgithub@126.com>
2017-02-05 14:48:10 +0800

    杨帆提交的

--- app/src/main/java/wx/eigle/yf/com/mafengwo_app/Entity/xx.java
+++ app/src/main/java/wx/eigle/yf/com/mafengwo_app/Entity/xx.java
@@ -1,8 +1,10 @@
 package wx.eigle.yf.com.mafengwo_app.Entity;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Administrator on 2017/2/5.
  */
 
-public class xx {
+public class xx extends AppCompatActivity{
 }
